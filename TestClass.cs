e2d2100c60ba468da1fee2b33373f3c39b2348ee
icfuller <71522781+icfuller@users.noreply.github.com>
2021-09-11 14:46:51 -0500

    -Added "TestClass.cs" to make sure the project is working.Update README.md

---
+++ TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP21_Final_Project
+{
+    class TestClass
+    {
+        //This class is a test and will be removed later
+    }
+}
